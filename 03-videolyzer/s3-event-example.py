bbccb4dc0df2f5fe5f5df74c7a9e097cf5f57290
darryl candiff <darryl@ccservices.nl>
2019-02-18 11:33:11 +0100

    Trigger rekognition job in lambda function

---
+++ 03-videolyzer/s3-event-example.py
@@ -0,0 +1,6 @@
+event['Records'][0]['s3']
+event['Records'][0]['s3']['s3SchemaVersion']
+event['Records'][0]['s3']['bucket']['name']
+event['Records'][0]['s3']['object']['key']
+import urllib
+urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,4 +1,25 @@
+import urllib
+
+import boto3
+
+def start_label_detection(bucketname, key):
+    rekognition_client = boto3.client('rekognition')
+    response = rekognition_client.start_label_detection(
+        Video={
+            'S3Object': {
+                'Bucket': bucketname,
+                'Name': key
+            }
+        })
+
+    print (response)
+
+    return
+
 def start_processing_video(event, context):
-    print(event)
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        start_label_detection(bucket_name, urllib.parse.unquote_plus(key))
 
     return
