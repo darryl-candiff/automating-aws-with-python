0e76a40a1a0b52d9fcc075cf2a70cac45478fe05
darryl candiff <darryl@ccservices.nl>
2019-02-18 19:37:06 +0100

    last version

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,5 +1,5 @@
+import os
 import urllib
-
 import boto3
 
 def start_label_detection(bucketname, key):
@@ -10,9 +10,19 @@ def start_label_detection(bucketname, key):
                 'Bucket': bucketname,
                 'Name': key
             }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
-
-    print (response)
+    print("Bucket name:",bucketname)
+    print("Key:", key)
+    print(rekognition_client)
+    print( os.environ['REKOGNITION_SNS_TOPIC_ARN'])
+    print(os.environ['REKOGNITION_ROLE_ARN'])
+    print(response.keys())
+    print(response['JobId'])
+    print(response['ResponseMetadata'])
 
     return
 
@@ -21,5 +31,11 @@ def start_processing_video(event, context):
         bucket_name = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
         start_label_detection(bucket_name, urllib.parse.unquote_plus(key))
+
+    return
+
+def handle_label_detection(event, context):
+
+    print(event)
 
     return
